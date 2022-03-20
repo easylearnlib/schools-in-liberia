b38847adfa67b7c6864a3244a2f98466f19fd561
Solo Dolley <solo@cocreators.ee>
2022-03-21 01:18:49 +0200

    fix index page redirect issue

--- next.config.js
+++ next.config.js
@@ -2,14 +2,12 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 
   async redirects() {
     return [
       {
-        source: process.env.NEXT_PUBLIC_BASE_PATH,
-        destination: `${process.env.NEXT_PUBLIC_BASE_PATH}/archives/1`,
+        source: "/",
+        destination: "/archives/1",
         permanent: true,
       },
     ];
