2719d7ed1f3f9be27adfb7b1c9850ad862617d32
omarudolley <58733599+omarudolley@users.noreply.github.com>
2022-10-16 11:57:10 +0000

    Deploying to gh-pages from @ omarudolley/schools-in-liberia@3b6167ae1424fe3a5cdabe10ea9266014de629a2 🚀

--- _next/static/ev55QhvKxtBizRR_Ci77t/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a80d973ae10717c6.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],"/schools/[name]":["static/chunks/pages/schools/[name]-e4533255bd61ea21.js"],sortedPages:["/","/_app","/_error","/schools/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
